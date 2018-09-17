28b93397d2fc8190f4263ad36bf8e37a10ba16e5
Wisdom Anthony <wizitendo10@gmail.com>
2018-09-17 23:56:59 +0100

    started work on frontend public view

---
+++ wp-content/themes/reunite/archive-fims-type.php
@@ -0,0 +1,30 @@
+<?php
+    $args = array(
+      'post_type' => 'films',
+      'tax_query' => array(
+        array(
+          'taxonomy' => 'film_category',
+          'field' => 'slug',
+          'terms' => 'films'
+        )
+      )
+    );
+    $filmss = new WP_Query( $args );
+    if( $films->have_posts() ) {
+      while( $films->have_posts() ) {
+        $films->the_post();
+        ?>
+          <h1><?php the_title() ?></h1>
+          <div class='content'>
+            <?php the_content() ?>
+             <?php $price = get_post_meta( get_the_ID(), 'ticket_price', true ); 
+             ?>
+            
+          </div>
+        <?php
+      }
+    }
+    else {
+      echo 'Oh ohm no films!';
+    }
+  ?>
